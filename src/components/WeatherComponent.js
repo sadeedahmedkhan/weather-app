60b686f1c40153e1a4929ea54cd66290b1f4b524
SadeedAhmedKhan <sadeedahmedkhan.official@gmail.com>
2023-05-06 20:58:52 +0100

    Add basic functionality

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './Weather.module.css';
 
-const WeatherComponent = () => {
+const DAYS = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const WeatherComponent = (props) => {
+  const { date, temperature, windSpeed, description } = props;
+
   return (
     <section className={styles.main_container}>
-      <h3>Day</h3>
-      <p>Date</p>
-      <div>image</div>
-      <p>Description</p>
-      <p>Temperature (C): </p>
-      <p>Wind (Km/h):</p>
+      <h3>{DAYS[new Date(date).getDay()]}</h3>
+      <p>{date}</p>
+      <div className={styles.image}>image</div>
+      <p>{description}</p>
+      <p>Temperature (C): {temperature}</p>
+      <p>Wind (Km/h): {windSpeed}</p>
     </section>
   );
 };
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import axios from 'axios';
 import Header from '../header/Header';
 import WeatherComponent from './../components/WeatherComponent';
 import styles from './Home.module.css';
+import { CODES } from '../constants/WeatherCodes';
 
 const Home = () => {
+  const [search, setSearch] = useState('');
+  const [weatherResults, setWeatherResults] = useState(null);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handlePressEnter = async (e) => {
+    if (
+      e.key === 'Enter' &&
+      e.target.value.length > 1 &&
+      e.target.value !== ' '
+    ) {
+      const request = await axios
+        .get(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${search}&count=1&language=en&format=json`
+        )
+        .then((response) => response.data.results[0]);
+      await axios
+        .get(
+          `https://api.open-meteo.com/v1/forecast?latitude=${request.latitude}&longitude=${request.longitude}&forecast_days=6&daily=temperature_2m_max,weathercode,windspeed_10m_max&timezone=${request.timezone}`
+        )
+        .then((response) => {
+          setWeatherResults(response.data);
+        });
+    }
+  };
+
+  const formatWeatherData = useMemo(() => {
+    let weatherData = [];
+    if (weatherResults !== null) {
+      for (let i = 1; i < 6; i++) {
+        weatherData.push({
+          date: weatherResults.daily.time[i],
+          temperature: weatherResults.daily.temperature_2m_max[i],
+          windSpeed: weatherResults.daily.windspeed_10m_max[i],
+          description: CODES.get(weatherResults.daily.weathercode[i]),
+        });
+      }
+    }
+    return weatherData;
+  }, [weatherResults]);
+
   return (
     <div>
       <Header />
       <div className={styles.search_container}>
-        <input className={styles.search_input} placeholder='Search here...' />
+        <input
+          autoFocus
+          className={styles.search_input}
+          value={search}
+          onChange={handleSearchChange}
+          placeholder='Search here...'
+          onKeyDown={handlePressEnter}
+        />
       </div>
       <section className={styles.weather_section}>
-        <WeatherComponent />
+        {weatherResults !== null &&
+          formatWeatherData.map((data) => (
+            <WeatherComponent
+              key={data.date}
+              date={data.date}
+              temperature={data.temperature}
+              windSpeed={data.windSpeed}
+              description={data.description}
+            />
+          ))}
       </section>
     </div>
   );
